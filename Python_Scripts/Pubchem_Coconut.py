8c3986afe6ce522fd88c559f4b1805ded6b463eb
Kohulan <kohulan.rajan@uni-jena.de>
2020-07-01 10:56:53 +0200

    Updated to handle compounds without synonyms

--- Python_Scripts/Pubchem_Coconut.py
+++ Python_Scripts/Pubchem_Coconut.py
@@ -12,17 +12,16 @@ import pubchempy as pcp
 import argparse
 
 parser = argparse.ArgumentParser(description="Fetch Pubchem data using cids")
-
 # Input Arguments
 parser.add_argument(
-	'--input',
-	help = 'Enter the input filename',
-	required = True
-	)
+    '--input',
+    help = 'Enter the input filename',
+    required = True
+    )
 parser.add_argument(
-	'--output',
-	help = 'Enter the output filename as desired',
-	required = True
+    '--output',
+    help = 'Enter the output filename as desired',
+    required = True
 )
 args= parser.parse_args()
 
@@ -42,37 +41,38 @@ def main():
             cids.append(cid)
             coconut_ids.append(coconut_id)
 
-    print("Total Pubchem IDs: ",len(cids)," and total coconut_ids: ",len(coconut_ids)) # Just to cross check the data
+    print("Total Pubchem IDs: ",len(cids)," and total coconut_ids: ",len(coconut_ids))
 
 
     for i in range(len(cids)):
         try:
             retrieve = pcp.Compound.from_cid(cids[i])
-            f.write(cids[i]+"\t"+str(coconut_ids[i])+"\t"+str(retrieve.synonyms[0])+
-            	"\t"+retrieve.iupac_name+"\t"+get_cas(retrieve)+"\t"+str(retrieve.synonyms) +"\n")
-            f.flush()
-
+            if len(retrieve.synonyms)>0:
+                f.write(cids[i]+"\t"+str(coconut_ids[i])+"\t"+str(retrieve.synonyms[0])+
+                    "\t"+retrieve.iupac_name+"\t"+get_cas(retrieve)+"\t"+str(retrieve.synonyms) +"\n")
+                f.flush()
+            else:
+                f.write(cids[i]+"\t"+str(coconut_ids[i])+"\t"+
+                    "\t"+retrieve.iupac_name+"\n")
+                f.flush()
         except Exception as e:
             print(e,cids[i])
             coconut_ids_unprocessed.append(coconut_ids[i])
-    
-    if len(coconut_ids_unprocessed)>0:
-        for i in range(len(coconut_ids_unprocessed)):
-            f.write("Unprocessed ID: "+str(coconut_ids_unprocessed[i]))
+
+    for i in range(len(coconut_ids_unprocessed)):
+        f.write("Unprocessed ID: "+str(coconut_ids_unprocessed[i]))
 
     f.close()
 
-
-# Fuction to retrive CAS Registry Numbers
 def get_cas(retrieve):
-	cas_iter = []
-	cas_syn = retrieve.synonyms
-	for i in range(len(cas_syn)):
-		cas_match = re.match('(\d{2,7}-\d\d-\d)', cas_syn[i])
-		if cas_match:
-			cas_iter.append(cas_match.group(1))
+    cas_iter = []
+    cas_syn = retrieve.synonyms
+    for i in range(len(cas_syn)):
+        cas_match = re.match('(\d{2,7}-\d\d-\d)', cas_syn[i])
+        if cas_match:
+            cas_iter.append(cas_match.group(1))
 
-	return str(cas_iter)
+    return str(cas_iter)
 
 if __name__ == '__main__':
     main()
